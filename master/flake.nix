a29cad1ea00e6f6b5c2d3262e79f208a7eefa2b3
hugosenari <hugosenari@gmail.com>
2022-03-27 18:18:00 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-hyperscript-master.ref   = "refs/heads/master";
   inputs.src-hyperscript-master.owner = "schneiderfelipe";
   inputs.src-hyperscript-master.repo  = "hyperscript";
+  inputs.src-hyperscript-master.dir   = "";
   inputs.src-hyperscript-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
